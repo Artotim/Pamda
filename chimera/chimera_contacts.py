b18ffe7b14cd45a8be6081650fb3a0bbaaaa9b6c
Arthur <arthurpfonseca3k@hotmail.com>
2020-12-20 23:50:06 -0300

    Added error for model not found

--- chimera/chimera_contacts.py
+++ chimera/chimera_contacts.py
@@ -6,9 +6,11 @@ import sys
 from chimera import runCommand as rc
 
 
-def create_output_files(out):
-    map_file = out + "contact/contact_map.csv"
-    count_file = out + "contact/contact_count.csv"
+def create_output_files(out_path):
+    """Create csv files for writing"""
+
+    map_file = out_path + "contact/contact_map.csv"
+    count_file = out_path + "contact/contact_count.csv"
 
     with open(map_file, "w") as all_contacts:
         all_contacts.write("frame;atom1;atom2;overlap;distance\n")
@@ -17,17 +19,25 @@ def create_output_files(out):
         contacts_count.write("frame;contacts\n")
 
 
-def check_for_model(model, count):
+def check_for_model(model, count, loops):
+    """Check if model is ready"""
+
+    if loops == 50:
+        raise Exception("Contact files not being created")
+
     if not os.path.exists(model):
         print("Model not found! Awaiting...")
         time.sleep(2)
-        return check_for_model(model, count)
+        loops = loops + 1
+        return check_for_model(model, count, loops)
     else:
         print("Computing model", count, end=".")
 
 
-def create_contact_map(out, count, main_chain, peptide):
-    file_name = out + "contact/overlaps_" + count + ".txt"
+def create_contact_map(out_path, count, main_chain, peptide):
+    """Get contact info with chimera"""
+
+    file_name = out_path + "contact/overlaps_" + count + ".txt"
     cmd = "findclash :." + main_chain + " test :." + peptide + \
           " overlap -0.4 hbond 0.0 namingStyle simple save " + file_name
     rc(cmd)
@@ -35,14 +45,18 @@ def create_contact_map(out, count, main_chain, peptide):
 
 
 def read_contacts(contact_map):
+    """Read contact infos"""
+
     with open(contact_map, "r") as contacts_now:
         file = contacts_now.readlines()
     print(".", end="")
     return file
 
 
-def get_contact_number(out, contact_file, count):
-    count_file = out + "contact/contact_count.csv"
+def get_contact_number(out_path, contact_file, count):
+    """Count contact hits"""
+
+    count_file = out_path + "contact/contact_count.csv"
     contact_number = re.findall(r'\d+', contact_file[6])
 
     with open(count_file, "a") as contacts_count:
@@ -50,8 +64,10 @@ def get_contact_number(out, contact_file, count):
     print(".", end="")
 
 
-def get_contact_list(out, contact_file, count):
-    map_file = out + "contact/contact_map.csv"
+def get_contact_list(out_path, contact_file, count):
+    """Format contact map"""
+
+    map_file = out_path + "contact/contact_map.csv"
     line = 8
 
     while line < len(contact_file):
@@ -63,35 +79,39 @@ def get_contact_list(out, contact_file, count):
 
 
 def finnish(model, contact_map):
+    """Close chimera and remove old info"""
+
     rc("close all")
     os.remove(model)
     os.remove(contact_map)
 
 
-def look_for_models(init, final, out, cci, main_chain, peptide):
+def main(init, final, out_path, cci, main_chain, peptide):
+    """Main routine"""
+
     count = init + cci
 
     while count <= final:
         count_str = str(count)
 
         # Await pdb file
-        model = out + 'contact/contact_model_' + count_str + '.pdb'
-        check_for_model(model, count_str)
+        model = out_path + 'contact/contact_model_' + count_str + '.pdb'
+        check_for_model(model, count_str, 0)
 
         # Open model on chimera
         rc("open " + model)
 
         # Create contact map
-        contact_map = create_contact_map(out, count_str, main_chain, peptide)
+        contact_map = create_contact_map(out_path, count_str, main_chain, peptide)
 
         # Read contact maps file
         contact_file = read_contacts(contact_map)
 
         # Get number of contact for this frame
-        get_contact_number(out, contact_file, count_str)
+        get_contact_number(out_path, contact_file, count_str)
 
         # Get contact list for this frame
-        get_contact_list(out, contact_file, count_str)
+        get_contact_list(out_path, contact_file, count_str)
 
         # Close pdb and remove files
         finnish(model, contact_map)
@@ -101,13 +121,9 @@ def look_for_models(init, final, out, cci, main_chain, peptide):
     print("Finished!")
 
 
-def main():
+if __name__ == '__main__':
     print("Starting analysis...")
     out = sys.argv[1]
     create_output_files(out)
 
-    look_for_models(int(sys.argv[2]), int(sys.argv[3]), out, int(sys.argv[4]), sys.argv[5], sys.argv[6])
-
-
-if __name__ == '__main__':
-    main()
+    main(int(sys.argv[2]), int(sys.argv[3]), out, int(sys.argv[4]), sys.argv[5], sys.argv[6])
