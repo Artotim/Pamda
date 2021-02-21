59599bcecadb2115603d029622cc3639aea4b403
Arthur <arthurpfonseca3k@hotmail.com>
2021-02-20 21:19:49 -0300

    Added option for catalytic site labels

--- plots/plot_contact_map.r
+++ plots/plot_contact_map.r
@@ -3,6 +3,7 @@ dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)  # create personal libra
 if (!require('ggplot2')) install.packages('ggplot2', lib = Sys.getenv("R_LIBS_USER"), repos = "https://cloud.r-project.org/"); library('ggplot2')
 if (!require('tidyr')) install.packages('tidyr', lib = Sys.getenv("R_LIBS_USER"), repos = "https://cloud.r-project.org/"); library('tidyr')
 if (!require('tidyverse')) install.packages('tidyverse', lib = Sys.getenv("R_LIBS_USER"), repos = "https://cloud.r-project.org/"); library('tidyverse')
+if (!require('stringr')) install.packages('stringr', lib = Sys.getenv("R_LIBS_USER"), repos = "https://cloud.r-project.org/"); library('stringr')
 if (!require('extrafont')) {
     install.packages('extrafont', lib = Sys.getenv("R_LIBS_USER"), repos = "https://cloud.r-project.org/")
     library('extrafont')
@@ -12,6 +13,7 @@ if (!require('extrafont')) {
 library('ggplot2')
 library('tidyr')
 library('tidyverse')
+library('stringr')
 library('extrafont')
 
 
@@ -21,6 +23,11 @@ out.path <- args[1]
 out.path <- paste0(out.path, "contact/")
 
 name <- args[2]
+
+
+# Resolve catalytic site
+catalytic <- data.frame(resn=str_extract(tail(args, -2), "[aA-zZ]+"), resi=str_extract(tail(args, -2), "[0-9]+"))
+catalytic <- if (length(catalytic != 0 )) catalytic else data.frame(resn=NaN, resi=NaN)
 
 
 # Load contact map
@@ -110,23 +117,38 @@ contact.all.hits <- contact.all.hits %>%
 all.subset <- contact.all.hits[!(contact.all.hits$count == 0),]
 
 
+# Create data for catalytic labels
+for (i in catalytic$resi){
+    if (!(i %in% all.subset$protein) && !is.na(i)){
+        append.resi <- data.frame(protein=i, peptide=min(peptide.first), count = 0)
+        all.subset <- rbind(all.subset, append.resi)
+    }
+}
+catalytic$label <-  with(catalytic, paste0(resi, '\n', resn))
+catalytic$label <- gsub("\nNA", "", catalytic$label)
+
+
+
 # Plot all graph
 out.name <- paste0(out.path, name, "_contact_map_all.png")
 
 cat("Ploting contact map.\n")
-plot <- ggplot(all.subset, aes(peptide, protein, fill = count)) +
-    geom_raster() +
+plot <- ggplot(all.subset, aes(peptide, protein)) +
+    geom_raster(aes(fill = count)) +
+    geom_hline(yintercept = catalytic$resi, color = "#b30000", size = 0.7, linetype = "dashed") +
+    geom_text(data=catalytic, aes_string(x=peptide.last + 0.7, y = "resi", label="label"), color="#b30000", size=4, lineheight = 1) +
+    geom_vline(xintercept = seq(1.5, peptide.length - 0.5, 1), lwd = 0.5, colour = "black") +
     scale_fill_gradient(low = "white", high = "red") +
     scale_y_discrete(breaks = unique(all.subset$protein)[c(FALSE, TRUE)]) +
     scale_x_discrete(breaks = peptide.first:peptide.last, labels = str_split(peptide.chain, " ")) +
     labs(title = "Contact per residue", x = "Peptide", y = "Convertase") +
+    coord_cartesian(clip = 'off') +
     theme_minimal() +
     theme(text = element_text(family = "Times New Roman")) +
     theme(plot.title = element_text(size = 36, hjust = 0.5)) +
     theme(axis.title = element_text(size = 24)) +
     theme(axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 12)) +
     theme(panel.grid.major.x = element_blank()) +
-    geom_vline(xintercept = seq(1.5, peptide.length - 0.5, 1), lwd = 0.5, colour = "black") +
     labs(fill = "Contacts #")
 
 ggsave(out.name, plot, width = 350, height = 150, units = 'mm', dpi = 320, limitsize = FALSE)
@@ -200,22 +222,32 @@ for (i in seq_along(contact.hits)) {
     step.subset <- contact.hits[[i]][contact.hits[[i]]$protein %in% all.subset$protein,]
     step.subset$count[step.subset$count == 0] = NA
 
+    for (res in catalytic$resi){
+        if (!(res %in% all.subset$protein) && !is.na(res)){
+            append.resi <- data.frame(protein=res, peptide=min(peptide.first), count = 0)
+            step.subset <- rbind(step.subset, append.resi)
+        }
+    }
+
     cat("Ploting contact map for step", i, '\n')
-    plot <- ggplot(step.subset, aes(peptide, protein, fill = count)) +
-        geom_raster() +
+    plot <- ggplot(step.subset, aes(peptide, protein)) +
+        geom_raster(aes(fill = count)) +
+        geom_hline(yintercept = catalytic$resi, color = "#b30000", size = 0.7, linetype = "dashed") +
+        geom_text(data=catalytic, aes_string(x=peptide.last + 0.7, y = "resi", label="label"), color="#b30000", size=4, lineheight = 1) +
+        geom_vline(xintercept = seq(1.5, peptide.length - 0.5, 1), lwd = 0.5, colour = "black") +
         scale_fill_gradient(low = "white", high = "red", limits = max.range, na.value = "transparent") +
         scale_y_discrete(breaks = unique(all.subset$protein)[c(FALSE, TRUE)]) +
         scale_x_discrete(breaks = peptide.first:peptide.last, labels = str_split(peptide.chain, " ")) +
         labs(title = plot.title, x = "Peptide", y = "Convertase") +
+        coord_cartesian(clip = 'off') +
         theme_minimal() +
         theme(text = element_text(family = "Times New Roman")) +
         theme(plot.title = element_text(size = 36, hjust = 0.5)) +
         theme(axis.title = element_text(size = 24)) +
         theme(axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 12)) +
         theme(panel.grid.major.x = element_blank()) +
-        geom_vline(xintercept = seq(1.5, peptide.length - 0.5, 1), lwd = 0.5, colour = "black") +
         labs(fill = "Contacts #")
 
     ggsave(out.name, plot, width = 350, height = 150, units = 'mm', dpi = 320, limitsize = FALSE)
 }
-cat("Done.\n")
+cat("Done.\n\n")
