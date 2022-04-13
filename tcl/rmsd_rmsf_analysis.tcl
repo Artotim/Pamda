cfba1028ba31eb6124697848e1be19472b4bf5d1
artotim <arthurpfonseca3k@hotmail.com>
2022-04-12 21:29:49 -0300

    Fixed rmsd measuring

--- tcl/rmsd_rmsf_analysis.tcl
+++ tcl/rmsd_rmsf_analysis.tcl
@@ -1,35 +1,35 @@
 # RMSD RMSF Analysis
 
 
-proc retrieve_mol_info {init} {
+proc retrieve_mol_info {} {
     global mol residue_list all_atoms atoms_reference atoms_selected interaction main_chain peptide main_chain_atom main_chain_reference peptide_atom peptide_reference
 
 	puts "Retrieving molecule infos"
 
     set residue_list [[atomselect $mol "name CA"] get resid]
 	set all_atoms [uplevel "#0" [list atomselect $mol all]]
-    set atoms_reference [uplevel "#0" [list atomselect $mol "name CA" frame $init]]
+    set atoms_reference [uplevel "#0" [list atomselect $mol "name CA" frame 1]]
     set atoms_selected [uplevel "#0" [list atomselect $mol "name CA"]]
 
-	set main_chain_reference [uplevel "#0" [list atomselect $mol "name CA and chain $main_chain" frame $init]]
+	set main_chain_reference [uplevel "#0" [list atomselect $mol "name CA and chain $main_chain" frame 1]]
 	set main_chain_atom [uplevel "#0" [list atomselect $mol "name CA and chain $main_chain"]]
 
 	if {$interaction == true} {
-		set peptide_reference [uplevel "#0" [list atomselect $mol "name CA and chain $peptide" frame $init]]
+		set peptide_reference [uplevel "#0" [list atomselect $mol "name CA and chain $peptide" frame 1]]
 		set peptide_atom [uplevel "#0" [list atomselect $mol "name CA and chain $peptide"]]
 	}
 
 }
 
 
-proc create_res_dic {init} {
+proc create_res_dic {} {
     global mol reference_dict compare_dict residue_list
 
 	puts "Creating residue dicts"
 
     foreach residue $residue_list {
         dict set reference_dict $residue [uplevel "#0" [list \
-		    atomselect $mol "name CA and resid $residue and noh" frame $init]]
+		    atomselect $mol "name CA and resid $residue and noh" frame 1]]
     }
 
     foreach residue $residue_list {
@@ -105,10 +105,18 @@ proc measure_rmsf {} {
 }
 
 
-proc prepare_rmsd {} {
-    global init
+proc prepare_rmsd {dcd_path} {
+    global init mol wrapped
 
-    retrieve_mol_info $init
-    create_res_dic $init
+    if {$init == 0} {
+        mol addfile $dcd_path type dcd first $init last $init waitfor all molid $mol
+    } else {
+        mol addfile $dcd_path type dcd first [expr $init - 1] last $init waitfor all molid $mol
+    }
+
+    pbc_wrap frames_now $wrapped
+
+    retrieve_mol_info
+    create_res_dic
     create_rmsf_out_file
 }
