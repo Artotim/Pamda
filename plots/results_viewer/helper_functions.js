5eed8ce3dcba7b6b8a47161889ce0aeead98e149
artotim <arthurpfonseca3k@hotmail.com>
2022-10-25 17:14:09 -0300

    Reformatted results html code

--- plots/results_viewer/helper_functions.js
+++ plots/results_viewer/helper_functions.js
@@ -19,38 +19,3 @@ function escapeSpecial(text) {
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-
-function chooseModel(model) {
-    switch (model) {
-        case "i":
-        case "ul":
-            return "Crystal";
-        case "ap":
-            return "Alphafold";
-        case "sw":
-            return "Swiss";
-        case "t":
-            return "i-Tasser";
-    }
-}
-
-
-function generateDynamicTitle(dynamicName) {
-    const dynamicInfo = dynamicName.split("_");
-    const convertase = capitalizeFirstLetter(dynamicInfo[0]);
-    const model = chooseModel(dynamicInfo[1]);
-    const peptide_len = dynamicInfo[2];
-    const peptide_model = dynamicInfo[3].toUpperCase();
-
-    return `${convertase} ${model} + Peptide ${peptide_len}${peptide_model}`
-}
-
-
-function generateConvertaseTitle(convertase) {
-    const dynamicInfo = convertase.split("_");
-    const name = capitalizeFirstLetter(dynamicInfo[0]);
-    const peptide_len = dynamicInfo[1];
-
-    return `${name} + Pep${peptide_len}`
-}
